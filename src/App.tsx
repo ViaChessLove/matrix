491e2c537c3877988eeb07e5ebebc4d54b5ca247
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2022-07-01 18:54:20 +0500

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 import { GlobalStyle } from './globalStyles';
 import Home from './pages/Home';
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle/>
+      <Header/>
       <Routes>
         <Route index element={<Home/>}/>
       </Routes>
