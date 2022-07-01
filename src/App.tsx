5a060379b71cea92c4404cd4dab894dfbc9de01f
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2022-07-01 15:35:59 +0500

    hello, neo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
